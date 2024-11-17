c09b951e88abcdcb8392eb97e043720bdbe38a57
bigexperiment <ganesha.727@gmail.com>
2024-11-17 11:02:52 -0800

    update everest

--- src/treks/ManasluCircuit.js
+++ src/treks/ManasluCircuit.js
@@ -92,7 +92,7 @@ const ManasluCircuit = () => {
 };
 
 const InfoCard = ({ icon, title, value }) => (
-  <div className="bg-gray-50 p-4 rounded-lg flex items-center">
+  <div className="bg-gray-50 p-4 rounded-lg flex items-center test">
     <div className="text-indigo-500 mr-3">{icon}</div>
     <div>
       <h3 className="text-sm font-semibold text-gray-500">{title}</h3>
